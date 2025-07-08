bdbd8e03a66f03f38ce762abf081081ba8eaf6e8
Gustavo <76635390+pichee@users.noreply.github.com>
2025-07-08 20:52:49 -0300

    Update resposta.py

--- DataBase/0181_employees_earning_more_than_their_managers/resposta.py
+++ DataBase/0181_employees_earning_more_than_their_managers/resposta.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    chefes = employee[employee['managerId'].isnull()]
-    chefes['managerId'] = chefes['id'] 
-    novatabela = pd.merge(employee,chefes ,on ='managerId',how ='inner')
-    novatabela = novatabela[novatabela['salary_x']>novatabela['salary_y']]
-    novatabela = novatabela.rename(columns={'name_x':'Employee'})
+    novatabela = pd.merge(employee,employee,left_on='managerId',right_on='id',suffixes=('empregado', 'gerente'))
+    novatabela = novatabela[novatabela['salaryempregado']>novatabela['salarygerente']]
+    novatabela = novatabela.rename(columns={'nameempregado':'Employee'})
     return novatabela[['Employee']]
