d3de92ca21bf397df27524641286d7e6e8e9e9f6
Gustavo <76635390+pichee@users.noreply.github.com>
2025-07-08 20:03:18 -0300

    Delete Database/0175.sql

--- Database/0175.sql
+++
@@ -1,7 +0,0 @@
---https://leetcode.com/problems/combine-two-tables/
-import pandas as pd
-
-def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-   merge = pd.merge(person,address , on='personId',how ='left')
-   resultado = merge[['firstName','lastName','city','state']]
-   return resultado
